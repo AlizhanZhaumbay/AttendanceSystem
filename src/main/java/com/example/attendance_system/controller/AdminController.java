93989f7a4fab17aad8c144c22cd85de308863aa7
AlizhanZhaumbay <moneymanever@gmail.com>
2024-02-29 21:14:46 +0600

    admin

--- src/main/java/com/example/attendance_system/controller/AdminController.java
+++ src/main/java/com/example/attendance_system/controller/AdminController.java
@@ -1,17 +1,17 @@
 package com.example.attendance_system.controller;
 
 import com.example.attendance_system.model.Course;
-import com.example.attendance_system.model.Person;
+import com.example.attendance_system.dto.PersonDto;
 import com.example.attendance_system.repo.CourseRepository;
 import com.example.attendance_system.service.AdminService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/admin")
@@ -23,18 +23,37 @@ public class AdminController {
 
     @GetMapping("/students")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<List<Person>> getAllStudents(){
+    public ResponseEntity<List<PersonDto>> getAllStudents(){
         return ResponseEntity.ok(adminService.getAllStudents());
+    }
+
+    @GetMapping("/students/{student_id}")
+    public ResponseEntity<PersonDto> getOneStudent(@PathVariable("student_id") Integer studentId) {
+        Optional<PersonDto> optionalStudent = adminService.getStudentById(studentId);
+        return optionalStudent
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/teachers")
-    public ResponseEntity<List<Person>> getAllTeachers(){
+    public ResponseEntity<List<PersonDto>> getAllTeachers(){
         return ResponseEntity.ok(adminService.getAllTeachers());
     }
+
+    @GetMapping("/teachers/{teacher_id}")
+    public ResponseEntity<PersonDto> getOneTeacher(@PathVariable("teacher_id")
+                                                Integer teacherId) {
+        Optional<PersonDto> optionalTeacher = adminService.getTeacherById(teacherId);
+        return optionalTeacher
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 
     @GetMapping("/courses")
     public ResponseEntity<List<Course>> getAllCourses(){
         return ResponseEntity.ok(courseRepository.findAll());
     }
+
 }
--- src/main/java/com/example/attendance_system/service/AdminService.java
+++ src/main/java/com/example/attendance_system/service/AdminService.java
@@ -1,33 +1,52 @@
 package com.example.attendance_system.service;
 
+import com.example.attendance_system.dto.PersonDtoFactory;
+import com.example.attendance_system.dto.PersonDto;
 import com.example.attendance_system.model.Person;
-import com.example.attendance_system.repo.StudentRepository;
-import com.example.attendance_system.repo.TeacherRepository;
+import com.example.attendance_system.repo.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class AdminService {
 
-    private final StudentRepository studentRepository;
-    private final TeacherRepository teacherRepository;
+//    private final StudentRepository studentRepository;
+//    private final TeacherRepository teacherRepository;
 
-    public List<Person> getAllStudents() {
-        Optional<List<Person>> optionalList = studentRepository.findAllStudents();
-        return optionalList
-                .orElse(Collections.emptyList());
+    private final UserRepository userRepository;
+    public List<PersonDto> getAllStudents() {
+        Optional<List<Person>> optionalList = userRepository.findAllStudents();
+
+        return optionalList.map(students -> students
+                .stream()
+                .map(PersonDtoFactory::convert)
+                .collect(Collectors.toList())).orElse(Collections.emptyList());
     }
 
-    public List<Person> getAllTeachers() {
-        Optional<List<Person>> optionalList = teacherRepository.findAllTeachers();
-        return optionalList
-                .orElse(Collections.emptyList());
+    public List<PersonDto> getAllTeachers() {
+        Optional<List<Person>> optionalList = userRepository.findAllTeachers();
+
+        return optionalList.map(teachers -> teachers
+                .stream()
+                .map(PersonDtoFactory::convert)
+                .collect(Collectors.toList())).orElse(Collections.emptyList());
+
     }
 
 
+    public Optional<PersonDto> getStudentById(Integer studentId) {
+        return userRepository.findStudentById(studentId)
+                .map(PersonDtoFactory::convert);
+    }
+
+    public Optional<PersonDto> getTeacherById(Integer teacherId) {
+        return userRepository.findTeacherById(teacherId)
+                .map(PersonDtoFactory::convert);
+    }
 }
