bdd5b212477434de9503f3a1cb3fae8f2aa80bbd
Bushra Aksoy <bbxsra17@gmail.com>
2024-04-04 23:58:58 +0500

    added cors policy for localhost:3000

--- src/main/java/com/example/attendance_system/controller/AdminController.java
+++ src/main/java/com/example/attendance_system/controller/AdminController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping("/api/v1/admin")
 @RequiredArgsConstructor
@@ -47,10 +48,5 @@ public class AdminController {
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
-
-
-
-
-
 
 }
--- src/main/java/com/example/attendance_system/controller/CourseController.java
+++ src/main/java/com/example/attendance_system/controller/CourseController.java
@@ -6,13 +6,11 @@ import com.example.attendance_system.service.CourseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
